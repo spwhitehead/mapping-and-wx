0be7157d5b75b7d920340acdb725e35631101580
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-07 08:17:57 -0700

    Initial Commit
    Initializes first version of the files.

---
+++ map_with_wx.py
@@ -0,0 +1,44 @@
+import requests
+import re
+
+latitude = 48.15232724387619
+longitude = -92.66442225796172
+
+url = f"https://api.weather.gov/points/{latitude},{longitude}"
+
+response = requests.get(url=url)
+weather_data = response.json()
+# print(weather_data)
+
+forecast_city = weather_data["properties"]["relativeLocation"]["properties"]["city"]
+forecast_state = weather_data["properties"]["relativeLocation"]["properties"]["state"]
+forecast_url = weather_data["properties"]["forecastHourly"]
+response = requests.get(url=forecast_url)
+forecast = response.json()
+print()
+
+periods = forecast["properties"]["periods"]
+current_period = periods[0]
+
+current_temperature = current_period["temperature"]
+current_wind_speed = current_period["windSpeed"]
+current_wind_direction = current_period["windDirection"]
+weather_condition = current_period["shortForecast"]
+is_daytime = current_period["isDaytime"]
+print()
+print(f"Current weather in {forecast_city}, {forecast_state}")
+print()
+print(f"Current Temperature: \t\t{current_temperature}")
+print(f"Winds are coming from the: \t{current_wind_direction}")
+print(f"Winds are at: \t\t\t{current_wind_speed}")
+print(f"The weather is currently: \t{weather_condition}")
+
+
+def is_sunny(weather_condition):
+    if is_daytime and re.search(r"sunny", weather_condition, re.IGNORECASE):
+        print("Take your sunglasses")
+    else:
+        print("No sunglasses needed outside right now.")
+
+
+is_sunny(weather_condition)
