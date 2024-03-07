000ab562898f0b1826a6f4d1b811578cfc8524d6
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-07 09:29:54 -0700

    Updates get_location.py
    Fixes get_location.py to return the lat/long of an address

--- get_location.py
+++ get_location.py
@@ -3,15 +3,13 @@ import re
 
 
 def get_destination(destination_address: str) -> str:
-    destination_address = str(input("Enter your destination address: "))
     formatted_address = re.sub(
         r'\s+', '+', destination_address.replace(",", "")).strip()
-    return formatted_address
+    url = f"https://geocode.maps.co/search?q={formatted_address}&api_key=65e8c024164ff880722812epqe3caac"
+    response = requests.get(url=url)
+    destination_data = response.json()
+    return destination_data
 
 
-get_destination()
-
-url = f"https://geocode.maps.co/search?q={formatted_address}&api_key=65e8c024164ff880722812epqe3caac"
-
-response = requests.get(url=url)
-destination_data = response.json()
+get_destination(destination_address=str(
+    input("Enter your destination address: ")))
