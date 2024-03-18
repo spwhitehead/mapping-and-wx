e48853949667f66673671757794bc38e92098ef8
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-18 10:30:16 -0600

    Debugs get_dirctions.py

--- get_directions.py
+++ get_directions.py
@@ -7,11 +7,7 @@ def get_travel_time(api_key, start_coord, end_coord):
     # Initialize the ORS client with the API key read from file
     client = openrouteservice.Client(key=api_key)
 
-    start_coord = (geocode.start_location.latitude,
-                   geocode.start_location.longitude)
-    end_coord = (geocode.end_location.latitude, geocode.end_location.longitude)
-
-    routes = directions(client, start_coord, end_coord)
+    routes = directions(client, [start_coord, end_coord])
     duration_seconds = routes['routes'][0]['summary']['duration']
     duration_hours = duration_seconds / 3600
 
